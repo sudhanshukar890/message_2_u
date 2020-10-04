2ebe9f3893cffde1c7f8cece7e10660d06a43202
sudhanshukar890 <71970642+sudhanshukar890@users.noreply.github.com>
2020-10-04 17:10:56 +0530

    Add files via upload

---
+++ names/name.js
@@ -0,0 +1,51 @@
+var name=prompt('Write your name in block letters only?');
+if(name=="SUDHANSHU")
+{
+	document.write("You are a good person. You are confused.");
+}
+else if(name=="RUCHIKA")
+{
+	document.write("You are a good girl who is wiser more than her age. I love you.");
+}
+else if(name=="SMARIKA"){
+	document.write("I have seen many intelligent people on screen. But in my life, you are that girl. ");
+}
+else if(name=="PRAVEER") {
+	document.write("You are a very good programmer(means for real). You are a good human as well. I like talking to you on phone. Wishes for your bright future and  our endless conversations.");
+}
+else if(name=="AKSH") {
+	document.write("You are one of my best friends. Missing the days of school when we shared our tiffin together and have endless conversation about anything. ");
+}
+else if(name=="TUSHAR") {
+	document.write("Just waiting to enjoy that Gupta ke samose stuff with you in Lal Kurti. You made me realise that I am a good listener too cause you talk for too long and I enjoy it.");
+}
+else if(name=="AYASHKANT") {
+	document.write("Ptani Yeh Corona kab khatam hogaa tb saath me baithke college ko gaali karenge. bhot acha gaata hai tu.");
+	document.write("You are pretty much versatile for a course like engineering.");
+}
+else if(name=="JATIN") {
+	document.write("Happy to see you watch web series and movies nowadays coz then our chats are going to be more relatable.");
+	document.write("Waiting kb tu college jayega aur, hostel aur intro ke baare me btayega. Missing Chole Bhature badly. Har cheez me khana ghusane ki aadat hai meri.");
+}
+else if(name=="CHITTA") {
+	document.write("Hope you not see this. But I must tell you , I am a understanding child. Earlier I expected from you to be a perfect dad, but now I don't expext this from you.  ");
+	document.write("I don't need you to be perfect for me, because I'm not a perfect son too. bas thoda kam daanta kro. You're the most imperfectly perfect dad. I am seeing from many");
+	document.write("years you are doing everything possible for us. you are the only one .");
+}
+else if(name=="SANGEETA") {
+	document.write("I am observing this from a long time. You are trying constantly to fulfil our smallest wishes. I just want you to care about yourselves.");
+	document.write("Being a mother of three, I think you have completely lost your individuality. I don't want any woman to this to herself for others.");
+}
+else if(name=="SAI") {
+	document.write("You are a very helpful friend of mine. You helped me when I was completely unwell. Thankyou. Miss seeing you in class doing mischiefs.");
+	document.write("You are a pure soul. ab pta chal gya hoga kisne likha tha ye tere liye.");
+}
+else if(name=="SWEETY SINGH") {
+	document.write("Missing the days when we walk into your home just like ours. That shows the kind of strong bond I share with you.");
+	document.write("You are one of the most powerful woman I have ever seen. The way you manage everything is too good specially with \"Babu\".");
+	document.write("The world is becoming great day by day with the help of woman like you. You are great. Blessed to have you as my sister.");
+}
+else {
+	document.write("Hello "+name+",Close this page. Nothing for you.");
+}
+
